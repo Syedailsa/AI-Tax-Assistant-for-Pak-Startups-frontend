84fd1690809a5b2f4ad8ab4683c81f067d6133e8
Syedailsa <ilsasyeda24@gmail.com>
2025-10-26 21:57:18 +0500

    logo

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 border-b bg-primary text-primary-foreground shadow-sm">
+      
       <div className="mx-auto max-w-5xl px-4 py-3 flex items-center justify-between">
-        <Link href="/" className="font-semibold">
-          TaxEdu PK
-        </Link>
+        <div className="flex items-center gap-2">
+            <Image src="/favicon.ico" alt="logo" width={30} height={14} className="rounded-sm" />
+            <Link href="/" className="font-semibold">
+              TaxEdu PK
+            </Link>
+        </div>
         <nav className="flex items-center gap-2">
           <Button
             asChild
